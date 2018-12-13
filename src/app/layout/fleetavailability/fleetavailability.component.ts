51e9b323d6314b7dc598faa056821137623ee165
Jaco van der Kleij <jacovdkleij@gmail.com>
2018-12-13 16:40:04 +0100

    Date selector works

--- src/app/layout/fleetavailability/fleetavailability.component.ts
+++ src/app/layout/fleetavailability/fleetavailability.component.ts
@@ -5,6 +5,7 @@ import * as jwt_decode from 'jwt-decode';
 import * as Chart from 'chart.js';
 import { Router } from '@angular/router';
 import * as moment from 'moment';
+import { forEach } from '@angular/router/src/utils/collection';
 
 @Component({
     selector: 'app-users',
@@ -21,11 +22,14 @@ export class FleetavailabilityComponent implements OnInit {
     edit = false;
     turbineWarrenty;
     loaded = false;
+    selectedMonth = 'Last 2 weeks';
+    availableMonths = [];
 
     ngOnInit() {
         this.createLineChart();
         this.newService.getTurbineWarranty().subscribe(data => {
-            this.turbineWarrenty = data[3];
+            this.turbineWarrenty = data[3]; //TODO
+            this.getAvailableMonths();
             this.loaded = true;
         });
     }
@@ -42,12 +46,12 @@ export class FleetavailabilityComponent implements OnInit {
                     fill: false
                   }, {
                     data: [null, null, null, 130, 125, 90, 85, 83, 75, -5],
-                    label: 'expected after first 3 months',
+                    label: 'Expected after first 3 months',
                     borderColor: 'blue',
                     fill: false
                   }, {
                     data: [200, 180, 170, 130],
-                    label: 'Reality',
+                    label: 'Recorded weather days',
                     borderColor: 'black',
                     fill: false
                   }
@@ -90,8 +94,16 @@ export class FleetavailabilityComponent implements OnInit {
     }
 
     MatlabDateToJSDate(serial) {
-        const dateInt = moment((serial - 719529) * 864e5).format('DD-MM-YYYY');
-        return dateInt;
+        var dateInt = moment((serial - 719529) * 864e5);
+        if (this.selectedMonth == 'last 2 weeks') {
+            return dateInt.format('DD-MM-YYYY');
+        } else {
+            return dateInt.format('DD');
+        }
+    }
+
+    MatLabDateToMoment(serial) {
+        return moment((serial - 719529) * 864e5);
     }
 
     changeToNicename(name) {
@@ -104,5 +116,47 @@ export class FleetavailabilityComponent implements OnInit {
         } catch (Error) {
             return null;
         }
+    }
+
+    getAvailableMonths() {
+        var dateStart = this.MatLabDateToMoment(this.turbineWarrenty.startDate);
+        var dateEnd = this.MatLabDateToMoment(this.turbineWarrenty.stopDate);
+
+        while (dateEnd > dateStart || dateStart.format('M') === dateEnd.format('M')) {
+            if (dateStart < moment()) {
+                this.availableMonths.push(dateStart.format('MMM YYYY'));
+            }
+            dateStart.add(1, 'month');
+        }
+        this.availableMonths.push('Last 2 weeks');
+        this.availableMonths.reverse();
+    }
+
+    checkDateRange(Date) {
+        var date = this.MatLabDateToMoment(Date);
+        if (this.selectedMonth != 'Last 2 weeks') {
+            var stopDate = moment('01 ' + this.selectedMonth, 'DD MMM YYYY');
+            if (date.month() == stopDate.month() && date.year() == stopDate.year()) {
+                return true;
+            }
+        } else {
+            var lastTwoWeeks = this.turbineWarrenty.Dates.slice(Math.max(this.turbineWarrenty.Dates.length - 14, 1));
+            if (lastTwoWeeks.indexOf(Date) > -1) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    getSailDay(sailday) {
+        if (sailday == '_NaN_') {
+            return '-';
+        } else {
+            return sailday;
+        }
+    }
+
+    setMonth(val) {
+        this.selectedMonth = val;
     }
 }
