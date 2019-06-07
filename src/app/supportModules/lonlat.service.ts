cb579dca39ce637af32278525d54269dd12f2b47
ErwinBMO <erwin@bmo-offshore.com>
2019-06-07 15:12:31 +0200

    Resolved bug where lon/lat were send as vertical / horizontal arrays from matlab (Previously resulted in route not showing for certain CTVs)

--- src/app/supportModules/lonlat.service.ts
+++ src/app/supportModules/lonlat.service.ts
@@ -6,14 +6,21 @@ import { Injectable } from '@angular/core';
   export class LonlatService{
     
     lonlatarrayToLatLngArray(input){
-        var latlngArray: Array<google.maps.LatLng> = []
-        var latlng
+        var latlngArray: Array<google.maps.LatLng> = [];
+        var latlng;
         input.lon.forEach((lon, index) => {
-            latlng = {
-                lat: input.lat[index][0],
-                lng:lon[0]
+            if (input.lat[index][0]) {
+                latlng = {
+                    lat: input.lat[index][0],
+                    lng: lon[0]
+                };
+            } else {
+                    latlng = {
+                        lat: input.lat[index],
+                        lng: lon
+                    }
             };
-            latlngArray[index] = latlng;
+            latlngArray.push(latlng);
         });
         return latlngArray
     }
