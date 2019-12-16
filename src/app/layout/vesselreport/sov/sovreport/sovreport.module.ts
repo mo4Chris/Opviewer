d7dc86aec282290927aa06a761dbffa17a809ea9
Chris <c_de_vos8@hotmail.com>
2019-12-16 16:48:44 +0100

    temp store for merge

--- src/app/layout/vesselreport/sov/sovreport/sovreport.module.ts
+++ src/app/layout/vesselreport/sov/sovreport/sovreport.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ChartsModule as Ng2Charts } from 'ng2-charts';
 import { CalculationService } from '../../../../supportModules/calculation.service';
+import { AutosizeModule } from 'ngx-autosize';
 
 @NgModule({
   imports: [
     Ng2Charts,
-    CommonModule
+    CommonModule,
+    AutosizeModule
   ],
   providers: [CalculationService]
 })
