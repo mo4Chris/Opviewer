d5d5638d67f8623489d8ff7ab9ec167d71d94a7f
Erwin <erwin@bmo-offshore.com>
2020-01-16 11:28:35 +0100

    Fixed the dpr input component - both readonly and for vessel master

--- src/app/layout/reports/dpr/sov/sov-dpr-input/sov-dpr-input-readonly/sov-dpr-input-readonly.component.ts
+++ src/app/layout/reports/dpr/sov/sov-dpr-input/sov-dpr-input-readonly/sov-dpr-input-readonly.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, OnChanges } from '@angular/core';
 
 @Component({
   selector: 'app-sov-dpr-input-readonly',
   templateUrl: './sov-dpr-input-readonly.component.html',
-  styleUrls: ['./sov-dpr-input-readonly.component.scss']
+  styleUrls: ['./sov-dpr-input-readonly.component.scss', '../../sovreport.component.scss']
 })
-export class SovDprInputReadonlyComponent implements OnInit {
+export class SovDprInputReadonlyComponent implements OnChanges {
+  @Input() standby: ReadonlyInput;
+  @Input() vesselNonAvailability: ReadonlyInput;
+  @Input() weatherDowntime: ReadonlyInput;
+  @Input() hoc: ReadonlyInput;
+  @Input() toolbox: ReadonlyInput;
+  @Input() liquids = {};
+  @Input() peopleOnVessel = {};
+  @Input() catering = {};
+  @Input() dp = {};
+  @Input() remarks = '';
 
   constructor() { }
 
-  ngOnInit() {
+  ngOnChanges() {
   }
 
+}
+
+interface ReadonlyInput {
+  Array: Array<number>;
+  Total?: number;
+  TotalOld?: number;
+  TotalNew?: number;
 }
--- src/app/layout/reports/reports-routing-module.ts
+++ src/app/layout/reports/reports-routing-module.ts
@@ -14,18 +14,9 @@ const routes: Routes = [
     { path: '**', component: ReportsComponent },
 ];
 
-// const subroutes: Routes = [
-//     { path: 'dpr', component: ReportsDprComponent, outlet: 'reports' }, // dpr/:mmsi did not work, not sure why
-//     { path: 'tables', component: TablesComponent, outlet: 'reports' },
-//     { path: 'longterm/:mmsi', component: LongtermComponent, outlet: 'reports' },
-//     { path: 'longterm', component: LongtermComponent, outlet: 'reports' },
-//     { path: 'siemens-kpi', component: SOVSiemensMonthlyKPIComponent, outlet: 'reports' },
-// ];
-
 @NgModule({
     imports: [
         RouterModule.forChild(routes),
-        // RouterModule.forRoot(subroutes),
     ],
     exports: [RouterModule]
 })
