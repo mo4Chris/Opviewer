31081c82483ba93fb459519c5d98db106b81800f
Chris <c_de_vos8@hotmail.com>
2020-10-08 15:20:06 +0200

    added extrameals for marinecontrollers to the interface as wellas updating the tests

--- src/app/layout/reports/dpr/sov/sov-dpr-input/sov-dpr-input.component.spec.ts
+++ src/app/layout/reports/dpr/sov/sov-dpr-input/sov-dpr-input.component.spec.ts
@@ -13,7 +13,7 @@ import { DatetimeService } from '@app/supportModules/datetime.service';
 import { SupportModelModule } from '@app/models/support-model.module';
 import { MockedUserServiceProvider } from '@app/shared/services/test.user.service';
 
-describe('SovDprInputComponent', () => {
+fdescribe('SovDprInputComponent', () => {
   let component: SovDprInputComponent;
   let fixture: ComponentFixture<SovDprInputComponent>;
   let saveSpy: jasmine.Spy;
