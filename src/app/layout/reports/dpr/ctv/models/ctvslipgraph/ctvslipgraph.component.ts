b292399449323113c39ce55193c453674e26b087
ErwinBMO <erwin@bmo-offshore.com>
2020-11-12 14:04:28 +0100

    Double import due to merge Slip graphs are now hidden if no data is provided
    (cherry picked from commit c53f63db04518f61f9907648c2c547f0abaa785f)

--- src/app/layout/reports/dpr/ctv/models/ctvslipgraph/ctvslipgraph.component.ts
+++ src/app/layout/reports/dpr/ctv/models/ctvslipgraph/ctvslipgraph.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, OnChanges, ViewChild, ElementRef, AfterViewInit, ChangeDetectionStrategy } from '@angular/core';
+import { Component, Input, OnChanges, ViewChild, ElementRef, ChangeDetectionStrategy, ChangeDetectorRef } from '@angular/core';
 import * as Chart from 'chart.js';
 import * as ChartAnnotation from 'chartjs-plugin-annotation';
 import { CalculationService } from '@app/supportModules/calculation.service';
@@ -11,7 +11,7 @@ import { SettingsService } from '@app/supportModules/settings.service';
   styleUrls: ['./ctvslipgraph.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class CtvslipgraphComponent implements AfterViewInit, OnChanges {
+export class CtvslipgraphComponent implements OnChanges {
   @Input() index: number;
   @Input() transfer: any;
   @Input() vesselUtcOffset: number;
@@ -27,18 +27,19 @@ export class CtvslipgraphComponent implements AfterViewInit, OnChanges {
     white: "rgba(0,0, 0, 0)",
   }
 
+  public hidden = false;
+
   constructor(
     private calcService: CalculationService,
     private dateService: DatetimeService,
     private settings: SettingsService,
+    private ref: ChangeDetectorRef,
   ) {
   }
 
-
-  ngAfterViewInit() {
-  }
-
   ngOnChanges() {
+    this.hidden = false;
+    this.ref.detectChanges();
     this.context = (<HTMLCanvasElement> this.canvas.nativeElement).getContext('2d');
     const localOffset = this.settings.localTimeZoneOffset;
     this.utcOffset = Math.round(this.settings.getTimeOffset(this.vesselUtcOffset) - localOffset || 0);
@@ -123,6 +124,9 @@ export class CtvslipgraphComponent implements AfterViewInit, OnChanges {
           console.error('Could not get 2d context!')
         }
       }
+    } else {
+      this.hidden = true;
+      this.ref.detectChanges();
     };
   }
 
