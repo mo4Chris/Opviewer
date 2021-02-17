136c2affab475c57d787b9cda9c547933344f52f
ErwinMO4 <erwin@mo4.online>
2021-02-17 14:01:35 +0100

    Comment Chris

--- src/app/supportModules/alert.service.spec.ts
+++ src/app/supportModules/alert.service.spec.ts
@@ -1,7 +1,7 @@
 import { TestBed } from '@angular/core/testing';
 import { AlertService } from './alert.service';
 
-fdescribe('AlertService', () => {
+describe('AlertService', () => {
   let service: AlertService;
   beforeEach(() => {
     TestBed.configureTestingModule({});
