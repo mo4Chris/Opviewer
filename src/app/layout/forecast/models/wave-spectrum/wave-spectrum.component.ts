52c6376c48230b89bbc874c2d92b2c887afd5c08
ErwinMO4 <erwin@mo4.online>
2021-04-16 11:06:44 +0200

    Added unit tests and fixed axes

--- src/app/layout/forecast/models/wave-spectrum/wave-spectrum.component.ts
+++ src/app/layout/forecast/models/wave-spectrum/wave-spectrum.component.ts
@@ -18,12 +18,9 @@ export class SovWaveSpectrumComponent implements OnChanges {
   @Input() wavePeakDir: number[];
   @Input() spectrum: number[][][];
 
-  // private Kmin = 0;
-  // private Kmax = 3.4438;
   private Kmax = 28.9;
-
   public parsedData: Plotly.Data[];
-  public spectrumIndex = 1;
+  public spectrumIndex = 0;
   public loaded = false;
   public PlotLayout: Partial<Plotly.Layout> = {
     // General settings for the graph
@@ -35,12 +32,14 @@ export class SovWaveSpectrumComponent implements OnChanges {
       title: 'Tx [1/s]',
       showgrid: false,
       zeroline: false,
+      range: [-1.15 * this.Kmax, 1.15*this.Kmax]
     },
     yaxis: {
       visible: false,
       title: 'Ty [1/s]',
       showgrid: false,
       zeroline: false,
+      range: [-1.15 * this.Kmax, 1.15*this.Kmax]
     },
     radialaxis: {
       'visible': false,
@@ -117,23 +116,32 @@ export class SovWaveSpectrumComponent implements OnChanges {
   ) {
   }
 
+  public get spectrumValid() {
+    if (!Array.isArray(this.spectrum)) return false;
+    return this.spectrum.some(_spec => {
+      return Array.isArray(_spec) && _spec.length>0
+    })
+  }
+
   ngOnChanges(): void {
     this.loaded = false;
-    if (this.k_x == null) return
+    if (this.k_x == null || !this.spectrumValid) return
     this.plotViaIndex();
     this.setSliderSteps();
     this.loaded = true;
   }
 
   plotViaIndex() {
-    const index = this.spectrumIndex-1 || 0;
+    const index = this.spectrumIndex;
     const delta = (this.k_x[1] - this.k_x[0]) / 2;
     const k = this.calcService.linspace(this.k_x[0], this.k_x[this.k_x.length-1], delta)
     const x = this.calcService.interp1(this.k_x, this.k_y, k)
     const y = this.calcService.interp1(this.k_y, this.k_y, k)
-    const z = this.calcService.interp2(this.k_x, this.k_y, this.spectrum[index], k, k).map(zz => zz.map(z => Math.log(1 + z)));
+    const z_temp = this.calcService.interp2(this.k_x, this.k_y, this.spectrum[index], k, k)
+    const z = z_temp.map(zz => zz.map(z => Math.log(1 + z)));
     const R2 = this.Kmax ** 2;
 
+    z.reverse();
     x.forEach((_x, ix) => {
       y.forEach((_y, iy) => {
         const r = _x**2 + _y**2;
@@ -203,11 +211,11 @@ export class SovWaveSpectrumComponent implements OnChanges {
   public onSliderChange(event: any) {
     const new_index = event.step._index;
     if (!(new_index >= 0)) return;
-    this.spectrumIndex = new_index + 1;
+    this.spectrumIndex = new_index;
     this.plotViaIndex()
   }
 
-  makeLine(angle, font = <any> {width: 1, color: "white"}) {
+  private makeLine(angle, font = <any> {width: 1, color: "white"}) {
     const x0 = Math.cos(angle * Math.PI / 180) * this.Kmax;
     const y0 = Math.sin(angle * Math.PI / 180) * this.Kmax;
     return {
@@ -219,7 +227,7 @@ export class SovWaveSpectrumComponent implements OnChanges {
       line: font
     }
   }
-  makeCircle(radius = this.Kmax, font = <any> {width: 5, color: "black"}) {
+  private makeCircle(radius = this.Kmax, font = <any> {width: 5, color: "black"}) {
     return {
       type: <'circle'> 'circle',
       x0: -radius,
@@ -229,8 +237,7 @@ export class SovWaveSpectrumComponent implements OnChanges {
       line: font
     }
   }
-
-  makeCircleTextAnnotation(value = 5, txt = '5s') {
+  private makeCircleTextAnnotation(value = 5, txt = '5s') {
     return {
       text: txt,
       showarrow: false,
@@ -244,8 +251,7 @@ export class SovWaveSpectrumComponent implements OnChanges {
       }
     }
   }
-
-  makeTextAnnotation(opts) {
+  private makeTextAnnotation(opts) {
     const defaults = {
       text: 'TEST',
       showarrow: false,
@@ -255,8 +261,7 @@ export class SovWaveSpectrumComponent implements OnChanges {
     }
     return {... defaults, ... opts};
   }
-
-  makeHeadingMarker(r: number[], ang_degs: number[], config: Partial<PlotData> = {}): Partial<PlotData> {
+  private makeHeadingMarker(r: number[], ang_degs: number[], config: Partial<PlotData> = {}): Partial<PlotData> {
     const theta = ang_degs.map(t => (90-t) * Math.PI / 180);
     let x0 = []; let y0 = [];
     theta.forEach((_theta, i) => {
