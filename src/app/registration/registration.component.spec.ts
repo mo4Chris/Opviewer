b45331c0e20e74d513748f0cdc041981af660ccc
Chris <c_de_vos8@hotmail.com>
2021-04-23 13:42:36 +0200

    added tests

--- src/app/registration/registration.component.spec.ts
+++ src/app/registration/registration.component.spec.ts
@@ -1,9 +1,9 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { SignupComponent } from './registration.component';
+import { RegistrationComponent } from './registration.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { FormsModule } from '@angular/forms';
 import { RouterTestingModule } from '@angular/router/testing';
-import { MockedCommonServiceProvider } from '@app/supportModules/mocked.common.service';
+import { MockedCommonService, MockedCommonServiceProvider } from '@app/supportModules/mocked.common.service';
 import { AppModule } from '@app/app.module';
 import { MockedUserServiceProvider } from '@app/shared/services/test.user.service';
 import { AuthService } from '@app/auth.service';
@@ -11,9 +11,9 @@ import { Router } from '@angular/router';
 import { mockedObservable } from '@app/models/testObservable';
 import { AlertService } from '@app/supportModules/alert.service';
 
-describe('SignupComponent', () => {
-  let component: SignupComponent;
-  let fixture: ComponentFixture<SignupComponent>;
+describe('RegistrationComponent', () => {
+  let component: RegistrationComponent;
+  let fixture: ComponentFixture<RegistrationComponent>;
   let authSpy: jasmine.Spy;
 
   beforeEach(async(() => {
@@ -25,7 +25,7 @@ describe('SignupComponent', () => {
         AppModule,
       ],
       declarations: [
-        SignupComponent
+        RegistrationComponent
       ],
       providers: [
         MockedCommonServiceProvider,
@@ -36,51 +36,35 @@ describe('SignupComponent', () => {
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(SignupComponent);
+    fixture = TestBed.createComponent(RegistrationComponent);
     component = fixture.componentInstance;
-    authSpy = spyOn(AuthService.prototype, 'registerUser').and.returnValue(mockedObservable({
+    authSpy = spyOn(AuthService.prototype, 'registerDemoUser').and.returnValue(mockedObservable({
       data: 'User registration tested!'
     }));
   });
 
-  it('should create as admin', () => {
-    component.permission.admin = true;
-    fixture.detectChanges();
+  it('should create', () => {
     expect(component).toBeTruthy();
-    expect(component.businessNames.length).toBeGreaterThan(0);
   });
 
-  it('should reroute w/out userCreate rights', () => {
-    component.permission.admin = false;
-    component.permission.userCreate = false;
-    const routerSpy = spyOn(Router.prototype, 'navigate');
-    fixture.detectChanges();
-    expect(component).toBeTruthy();
-    expect(routerSpy).toHaveBeenCalled();
-  });
-
-  it('should create w/ userCreate rights', async (done) => {
-    component.permission.admin = false;
-    component.permission.userCreate = true;
-    const routerSpy = spyOn(Router.prototype, 'navigate');
-    const onRegistrationSpy = spyOn(SignupComponent.prototype, 'onRegistration').and.callThrough();
-    const alertSpy = spyOn(AlertService.prototype, 'sendAlert');
-    fixture.detectChanges();
-    expect(component).toBeTruthy();
-    expect(routerSpy).not.toHaveBeenCalled();
+  it('should call correct service on register', () => {
 
     component.registerUserData = {
-      permissions: 'Vessel master',
-      email: 'test@test.nl',
-      client: 'test company 1',
-    };
+      email: 'test@testerson.nl',
+      password: 'test1234',
+      client: 'MO4',
+      confirmPassword: 'test1234',
+      name: 'Actual Human',
+      company: 'Picobello BV',
+      job_title: 'Opperbaas',
+      phoneNumber: '',
+      agreeDataPolicy: true
+    }
+
     const btn = fixture.nativeElement.querySelector('#register');
-    btn.dispatchEvent(new Event('click'));
-    await fixture.whenStable();
-    expect(onRegistrationSpy).toHaveBeenCalled();
+    btn.click();
+    fixture.detectChanges();
     expect(authSpy).toHaveBeenCalled();
-    expect(alertSpy).toHaveBeenCalled();
-    expect(routerSpy).toHaveBeenCalled();
-    done();
   });
+
 });
