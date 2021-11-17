57d757c38cb4341cc590f61979c2011ce487b2cb
sven-mo4 <93925115+sven-mo4@users.noreply.github.com>
2021-11-17 15:18:18 +0100

    feat(ctv-summary): Add fillable widgets

--- src/app/layout/reports/dpr/ctv/ctv-summary/ctv-summary.component.ts
+++ src/app/layout/reports/dpr/ctv/ctv-summary/ctv-summary.component.ts
@@ -14,6 +14,9 @@ import { SettingsService } from "@app/supportModules/settings.service";
 import { CTVGeneralStatsModel } from "../../models/generalstats.model";
 import { TokenModel } from "@app/models/tokenModel";
 
+// Types
+import { TimesQuarterHour } from "@app/supportModules/datetime.service";
+
 @Component({
   selector: "app-ctv-summary",
   templateUrl: "./ctv-summary.component.html",
@@ -57,6 +60,134 @@ export class CtvSummaryComponent implements OnChanges {
     "Oil Spill",
     "Other drills",
   ];
+
+  private _consumptionWidget: CtvConsumptionWidgetModel = {
+    fuel: {
+      used: 0,
+      remainingOnBoard: 0,
+      bunkered: 0,
+    },
+    water: {
+      used: 0,
+      remainingOnBoard: 0,
+      bunkered: 0,
+    },
+    shorePower: {
+      used: 0,
+      remainingOnBoard: 0,
+      bunkered: 0,
+    },
+  };
+
+  private _accessDayTypeOptions: selectOption<CtvAccessDayType>[] = [
+    { label: "Full access", value: CtvAccessDayType.FullAccess },
+    { label: "Half access", value: CtvAccessDayType.HalfAccess },
+    { label: "Weather day", value: CtvAccessDayType.WeatherDay },
+    { label: "Not required", value: CtvAccessDayType.NotRequired },
+    { label: "Maintenance", value: CtvAccessDayType.Maintenance },
+    { label: "Off contract", value: CtvAccessDayType.OffContract },
+  ];
+
+  private _accessDayType: CtvAccessDayType = CtvAccessDayType.Unselected;
+
+  private _hoursOnHire: number = 0;
+
+  private _weatherDownTimeDecidedByOptions: selectOption<CtvWeatherDowntimeDecidingParties>[] =
+    [
+      {
+        label: "Charterer",
+        value: CtvWeatherDowntimeDecidingParties.Charterer,
+      },
+      {
+        label: "Vessel master",
+        value: CtvWeatherDowntimeDecidingParties.VesselMaster,
+      },
+      {
+        label: "Joint decision",
+        value: CtvWeatherDowntimeDecidingParties.JointDecision,
+      },
+    ];
+
+  private _weatherDowntimeTimeOptions =
+    this.dateService.createTimesQuarterHour();
+
+  private _weatherDowntimeWidget: CtvWeatherDowntimeRowOptionsModel[] = [];
+
+  private _handleWeatherDowntimeAddLineClicked() {
+    this._weatherDowntimeWidget.push({
+      decidedBy: CtvWeatherDowntimeDecidingParties.Charterer,
+      from: "00:00",
+      to: "00:00",
+    });
+  }
+
+  private _handleWeatherDowntimeDeleteLastClicked() {
+    this._weatherDowntimeWidget.pop();
+  }
+
+  private _HSECountTotalAmountOf(input: CtvHSERowOptionModel[]): number {
+    const onlyNumbers = input.map(({ amount }) => amount);
+    const reducable = [0, ...onlyNumbers];
+    const totalAmount = reducable.reduce((prev, curr) => {
+      return prev + curr;
+    });
+    return totalAmount;
+  }
+
+  private _HSEEmptyRowOption(): CtvHSERowOptionModel {
+    return {
+      inputReason: "",
+      amount: 1,
+    };
+  }
+
+  private _HSESOCCards: CtvHSERowOptionModel[] = [];
+  private _HSESOCRemoveLastCardButtonEnabled = false;
+
+  private _handleSOCAddCardClicked() {
+    this._HSESOCCards.push(this._HSEEmptyRowOption());
+  }
+
+  private _handleSOCRemoveLastCardClicked() {
+    this._HSESOCCards.pop();
+  }
+
+  private _handleSOCRemoveCardWithIndex(index: number) {
+    this._HSESOCCards.splice(index, 1);
+  }
+
+  private _HSEToolboxTalks: CtvHSERowOptionModel[] = [];
+  private _HSEToolboxTalksRemoveLastButtonEnabled = false;
+
+  private _handleToolboxTalksAddClicked() {
+    this._HSEToolboxTalks.push(this._HSEEmptyRowOption());
+  }
+
+  private _handleToolboxTalksRemoveLastClicked() {
+    this._HSEToolboxTalks.pop();
+  }
+
+  private _handleToolboxTalksRemoveWithIndex(index: number) {
+    this._HSEToolboxTalks.splice(index, 1);
+  }
+
+  private _HSEDrills: CtvHSEDrillOptionModel[] = [];
+  private _HSEDrillsRemoveLastButtonEnabled = false;
+
+  private _handleDrillsAddClicked() {
+    this._HSEDrills.push({
+      ...this._HSEEmptyRowOption(),
+      involvedPassengers: false,
+    });
+  }
+
+  private _handleDrillsRemoveLastClicked() {
+    this._HSEDrills.pop();
+  }
+
+  private _handleDrillsRemoveWithIndex(index: number) {
+    this._HSEDrills.splice(index, 1);
+  }
 
   public fuelConsumedValue = "0 liter";
   public tripEfficiency = "N/a";
@@ -142,6 +273,10 @@ export class CtvSummaryComponent implements OnChanges {
   ) {
     return this.calcService.switchUnitAndMakeString(value, oldUnit, newUnit);
   }
+
+  trackByIndex(index: number, obj: any): any {
+    return index;
+  }
 }
 
 interface CtvEngineModel {
@@ -167,3 +302,49 @@ interface CtvGeneralInputStatsModel {
   passengers: any;
   customInput: string;
 }
+
+interface CtvConsumptionWidgetModel {
+  fuel: CtvConsumptionUnitOptionsModel;
+  water: CtvConsumptionUnitOptionsModel;
+  shorePower: CtvConsumptionUnitOptionsModel;
+}
+
+interface CtvConsumptionUnitOptionsModel {
+  used: number;
+  remainingOnBoard: number;
+  bunkered: number;
+}
+
+enum CtvAccessDayType {
+  FullAccess = "FULL_ACCESS",
+  HalfAccess = "HALF_ACCESS",
+  WeatherDay = "WEATHER_DAY",
+  NotRequired = "NOT_REQUIRED",
+  Maintenance = "MAINTENANCE",
+  OffContract = "OFF_CONTRACT",
+  Unselected = "UNSELECTED",
+}
+
+interface CtvWeatherDowntimeRowOptionsModel {
+  decidedBy: CtvWeatherDowntimeDecidingParties;
+  from: TimesQuarterHour;
+  to: TimesQuarterHour;
+}
+
+enum CtvWeatherDowntimeDecidingParties {
+  Charterer = "CHARTERER",
+  VesselMaster = "VESSEL_MASTER",
+  JointDecision = "JOINT_DECISION",
+  Unselected = "UNSELECTED",
+}
+
+interface CtvHSERowOptionModel {
+  inputReason: string;
+  amount: number;
+}
+
+interface CtvHSEDrillOptionModel extends CtvHSERowOptionModel {
+  involvedPassengers: boolean;
+}
+
+type selectOption<A> = { label: string; value: A };
