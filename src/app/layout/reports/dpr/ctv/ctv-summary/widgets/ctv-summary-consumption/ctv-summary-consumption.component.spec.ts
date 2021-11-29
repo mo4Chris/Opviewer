19887c3cb6441cc909f1ffd0ef9c80155b17c822
sven-mo4 <93925115+sven-mo4@users.noreply.github.com>
2021-11-29 13:31:07 +0100

    test(ctv-summary-consumption): Add test

--- src/app/layout/reports/dpr/ctv/ctv-summary/widgets/ctv-summary-consumption/ctv-summary-consumption.component.spec.ts
+++ src/app/layout/reports/dpr/ctv/ctv-summary/widgets/ctv-summary-consumption/ctv-summary-consumption.component.spec.ts
@@ -1,25 +1,46 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from "@angular/core/testing";
 
-import { CtvSummaryConsumptionComponent } from './ctv-summary-consumption.component';
+import { CtvSummaryConsumptionComponent } from "./ctv-summary-consumption.component";
 
-describe('CtvSummaryConsumptionComponent', () => {
+describe("CtvSummaryConsumptionComponent", () => {
   let component: CtvSummaryConsumptionComponent;
   let fixture: ComponentFixture<CtvSummaryConsumptionComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ CtvSummaryConsumptionComponent ]
-    })
-    .compileComponents();
+      declarations: [CtvSummaryConsumptionComponent],
+    }).compileComponents();
   });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(CtvSummaryConsumptionComponent);
     component = fixture.componentInstance;
+    component.data = {
+      fuel: {
+        startOfDay: 5,
+        used: 4,
+        bunkered: 3,
+        remainingOnBoard: 4,
+      },
+      water: {
+        startOfDay: 5,
+        used: 4,
+        bunkered: 3,
+        remainingOnBoard: 4,
+      },
+      shorePower: {
+        startOfDay: 5,
+        used: 4,
+        bunkered: 3,
+        remainingOnBoard: 4,
+      },
+    };
+    component.date = 737669;
+    component.mmsi = 123456789;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it("should create", () => {
     expect(component).toBeTruthy();
   });
 });
